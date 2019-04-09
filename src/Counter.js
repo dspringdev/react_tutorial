0bc1575446579672c1c0cf3c9d4c83b94e714545
dave <dave@daves-MacBook-Pro.local>
2019-04-09 07:41:29 -0400

    HandlingEvents

--- src/Counter.js
+++ src/Counter.js
@@ -7,15 +7,25 @@ function Counter(){
   const [count, setCount] = useState(0);
   const [tags, setTags] = useState(['tag1', 'tag2', 'tag3']);
   
-  function renderTags(){
-    if( tags.length === 0 ) return (<p>There are no tags!</p>);
+  function getCount(){
+    return(count === 0 ? "Zero" : count)
+  };
 
-    return <ul>{ tags.map(tag => <li key={ tag }>{ tag }</li>)}</ul>
+  function getBadgeStyle(){
+    let baseClass = "badge m-2 badge-"
+    baseClass += count === 0 ? "warning" : "primary"
+    return (baseClass)
+  }
+
+  function Increment(count){
+    return setCount(count + 1);
   }
 
   return (
     <React.Fragment>
-      { renderTags() }
+      <span className={ getBadgeStyle() }>{ getCount() }</span>
+      <button className="btn btn-secondary btn-sm" onClick={ () => { Increment(count) }}>Increment</button>
+      <ul>{ tags.map(tag => <li key={ tag }>{ tag }</li>)}</ul>
     </React.Fragment>
   );
 }
